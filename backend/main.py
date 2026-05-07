b6550f2aae2432b59fb7741a5c2c81ec9b4c5ef5
Asalxon <asalxonnishonova78@gmail.com>
2026-05-08 00:31:04 +0500

    Fix CORS for production

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Patient Calendar API 🏥")
 # Allow React frontend to talk to this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
